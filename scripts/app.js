cef17f42f3e5c1d50a184d13e7ded0806a853e8a
ChristmasLover12345 <madnes0205@gmail.com>
2024-12-03 14:35:08 -0800

    started on javascript

---
+++ scripts/app.js
@@ -0,0 +1,26 @@
+let dailyBtn = document.getElementById('dailyBtn');
+let weeklyBtn = document.getElementById('weeklyBtn');
+let monthlyBtn = document.getElementById('monthlyBtn');
+
+
+let workTimeNow = document.getElementById('workTimeNow');
+let workTimePast = document.getElementById('workTimePast');
+let playTimeNow = document.getElementById('playTimeNow');
+let playTimePast = document.getElementById('playTimePast');
+let studyTimeNow = document.getElementById('studyTimeNow');
+let studyTimePast = document.getElementById('studyTimePast');
+let exerciseTimeNow = document.getElementById('exerciseTimeNow');
+let exerciseTimePast = document.getElementById('exerciseTimePast');
+let socialTimeNow = document.getElementById('socialTimeNow');
+let socialTimePast = document.getElementById('socialTimePast');
+let selfCareTimeNow = document.getElementById('selfCareTimeNow');
+let selfCareTimePast = document.getElementById('selfCareTimePast');
+
+
+
+function getTime(){
+
+}
+
+
+window.onload = daily()
